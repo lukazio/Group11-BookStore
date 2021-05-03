c4b0dcab65707fe16862c5854ea770fbb4f3488c
LUKAZ <lukazlim@yahoo.com>
2021-05-03 17:47:01 +0800

    initialization

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+        <?php
+        require 'modules/link.php';
+        ?>
+    </head>
+    
+    <?php
+    require 'modules/dbconnect.php';
+    include 'modules/header.php';
+    ?>
+    
+    <body>
+    </body>
+    
+    <?php
+    include 'modules/footer.php';
+    ?>
+    
+</html>
---
+++ modules/link.php
@@ -0,0 +1,5 @@
+<script src="frameworks/jquery-3.5.1.min.js"></script>
+<script src="frameworks/popper.min.js"></script>
+<script src="frameworks/bootstrap.bundle.js"></script>
+<link rel="stylesheet" type="text/css" href="frameworks/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="frameworks/font-awesome-4.7.0/css/font-awesome.min.css">
