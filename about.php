39af8651ba2b2b1722e64512060d8edab055b282
vanessa <p17009280@student.newinti.edu.my>
2021-05-07 22:43:06 +0800

    add our story section

--- about.php
+++ about.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-        <link href="about.css" rel="stylesheet">
-
+        <link href="./about.css" rel="stylesheet">
+        <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+        <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
         <?php
         require 'modules/link.php';
         ?>
@@ -16,19 +17,32 @@
     ?>
 
     <body >
-        <section id="section1">
-            <div class="banner-bg-image"> 
-                            <a href="#section2" class="scroll-down" address="true"></a>
+        <!--about us banner-->
+        <div class="banner-bg-image"> 
+            <a href="#story" class="scroll-down" address="true"></a>
+        </div>
+        <div class="banner-text">
+            <h1 class="display-1">Who We Are</h1>
+            <h3>The Mondstadt Library Family</h3>
+        </div>
 
+        <!--about us our story-->
+        <div class="story-image" id="story">   
+            <div class="container">
+                <div class=" row">
+                    <div class="col-sm-6">
+                        <h1 class="display-3" style="padding-top:10% " data-aos="fade-up">Our Story</h1>
+                        <p class="lead" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-quad">The Mondstadt Library is an online bookstore with a mission to financially support local, independent bookstores.
+                            We believe that bookstores are essential to a healthy culture. 
+                            <br>They’re where authors can connect with readers, where we discover new writers, where children get hooked on the thrill of reading that can last a lifetime. They’re also anchors for our downtowns and communities. 
+                            <br>As more and more people buy their books online, we wanted to create an easy, convenient way for you to get your books and support bookstores at the same time.</p>
+                    </div>
+                    <script>AOS.init();</script>
+                </div>
             </div>
-            <div class="banner-text">
-                <h1 class="display-1">Who We Are</h1>
-                <h3>The Mondstadt Library Family</h3>
-            </div>
-        </section>
-        <section id="section2" class="ok" style="color: wheat">
-            <h1>title</h1>
-        </section>
+        </div>
+
+
     </body>
 
     <?php
