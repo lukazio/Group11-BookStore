74abff13c16618d6ae6b215d0c53a3725255864e
vanessa <p17009280@student.newinti.edu.my>
2021-05-09 01:22:25 +0800

    add css and image changes in about page

--- about.php
+++ about.php
@@ -2,8 +2,9 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>About</title>
         <link href="./about.css" rel="stylesheet">
+        <!--library for fade-up effect-->
         <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
         <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
         <?php
@@ -22,8 +23,8 @@
             <a href="#story" class="scroll-down" address="true"></a>
         </div>
         <div class="banner-text">
-            <h1 class="display-1" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-quad">Who We Are</h1>
-            <h3 data-aos="fade-up" data-aos-duration="2000" data-aos-easing="ease-in-quad">The Mondstadt Library Family</h3>
+            <h1 class="display-1" data-aos="fade-up" data-aos-duration="500">Who We Are</h1>
+            <h3 data-aos="fade-up" data-aos-duration="500">The Mondstadt Library Family</h3>
         </div>
 
         <!--about us our-story-->
@@ -31,8 +32,9 @@
             <div class="container">
                 <div class=" row">
                     <div class="col-sm-6">
-                        <h1 class="display-3" style="padding-top:10% " data-aos="fade-up">Our Story</h1>
-                        <p class="lead" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-quad">The Mondstadt Library is an online bookstore with a mission to financially support local, independent bookstores.
+                        <h1 class="display-3" style="padding-top:10% " data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-quad">Our Story</h1>
+                        <p class="lead" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-quad">
+                            The Mondstadt Library is an online bookstore with a mission to financially support local, independent bookstores.
                             We believe that bookstores are essential to a healthy culture. 
                             <br>They’re where authors can connect with readers, where we discover new writers, where children get hooked on the thrill of reading that can last a lifetime. They’re also anchors for our downtowns and communities. 
                             <br>As more and more people buy their books online, we wanted to create an easy, convenient way for you to get your books and support bookstores at the same time.</p>
@@ -46,11 +48,11 @@
             <div class="testimonial text-center">
                 <div class="container">
                     <div class="heading white-heading">
-                        <h3 class="display-4" data-aos="fade-up">Our Happy Customer</h3>
+                        <h3 class="display-3" data-aos="fade-up" data-aos-duration="800" data-aos-anchor-placement="bottom-bottom">Our Happy Customer</h3>
                     </div>
                     <div id="testimonial4" class="carousel slide testimonial4_indicators testimonial4_control_button thumb_scroll_x swipe_x" data-ride="carousel" data-pause="hover" data-interval="5000" data-duration="2000">
                         <div class="carousel-inner" role="listbox">
-                           <div class="carousel-item active">
+                            <div class="carousel-item active">
                                 <div class="testimonial4_slide">
                                     <img src="img/about-custa.jpg" class="img-circle img-responsive" />
                                     <p> I am so happy to find a site where I can shop for unusual items. I found what I wanted with ease and purchased it without a hitch. The packaging was phenomenal and my book arrived on time in perfect condition</p>
@@ -78,7 +80,7 @@
                         <a class="carousel-control-next" href="#testimonial4" data-slide="next">
                             <span class="carousel-control-next-icon"></span>
                         </a>
-                        
+
                     </div>
                 </div>
             </div>
