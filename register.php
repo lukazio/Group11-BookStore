3bf0c50e22c1c5bf1377b554d8126205ef1d8e55
LeeJunXian89 <58661547+LeeJunXian89@users.noreply.github.com>
2021-05-12 21:40:09 +0800

    Delete the unnecessary code

--- register.php
+++ register.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <title>Register</title>
         <link rel="stylesheet" href="modules/register.css">
-        <link href="Assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="Assets/css/bootstrap-theme.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        
 <?php require 'modules/link.php'; ?> 
 <style>
 :root {
