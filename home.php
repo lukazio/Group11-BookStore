45c06aee610e37a0aefeaeb368c4c9927e8193f1
Jayson6099 <p17009238@student.newinti.edu.my>
2021-06-03 14:28:28 +0800

    - Home Enhancements

--- home.php
+++ home.php
@@ -15,7 +15,7 @@
     include 'modules/header.php';
     ?>
     
-    <body>
+    <body class="body-bg">
         
         <!-- Banner -->       
         <div class="jumbotron background-tint">
@@ -142,17 +142,17 @@
         <div class="container">
             <div class="row">
                 <?php
-                    $sql = "SELECT title, author, picture, retail_price FROM book";
+                    $sql = "SELECT isbn, title, author, picture, retail_price FROM book";
                     $result = $conn->query($sql);
 
                     if ($result->num_rows > 0) {
                       while($row = $result->fetch_assoc()) {
                         echo
-                          "<div data-role=\"holder\" class=\"col-3 mb-5\">".
+                          "<div data-role=\"holder\" class=\"col-6 col-md-3 mb-5\">".
                               "<div class=\"card h-100\" data-role=\"book\">".
                                 "<img data-role=\"cardImgTop\" class=\"card-img-top\" src=\"".$row["picture"]."\" alt=\"".$row["title"]."\">".
                                 "<div data-role=\"cardBody\" class=\"card-body\">".    
-                                    "<div data-role=\"detail\" class=\"row\"><h5 class=\"col-12 card-title\">".$row["title"]."</h5></div>".
+                                    "<div data-role=\"detail\" class=\"row\"><h5 class=\"col-12\"><a class=\"card-title\" href=\"book_info.php?isbn=".$row["isbn"]."\">".$row["title"]."</a></h5></div>".
                                     "<div data-role=\"detail\" class=\"row\"><p class=\"col-12 card-text\">".$row["author"]."</p></div>".
                                     "<div data-role=\"detail\" class=\"row\"><p class=\"col-12 card-text\">RM ".$row["retail_price"]."</p></div>".
                                     "<div data-role=\"cartBtn\"><button class=\"cart-btn\"><i class=\"fa fa-shopping-cart\"></i> Add to Cart</button></div>".
