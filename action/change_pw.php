588212eec92dce51a5797beba0e4815a4df98707
Vincent <vincentang1999@gmail.com>
2021-06-17 13:51:59 +0800

    Change Password implementation done

--- action/change_pw.php
+++ action/change_pw.php
@@ -12,23 +12,6 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
         $new_pw = $_POST['new_password'];
         $new_retype_pw = $_POST['new_retype_password'];
 
-        $old_pw = md5($old_pw);
-        $new_pw = md5($new_pw);
-        $id = $_SESSION['user_id'];
-
-        $sql = "SELECT password FROM user WHERE user_id='$id' AND password='$old_pw'";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) === 1) {
-
-            $new_sql = "UPDATE user SET password='$new_pw' WHERE user_id='$id'";
-            mysqli_query($conn, $new_sql);
-            header("Location: ../profile.php?success=changesuccess");
-            exit();
-        } else {
-            header("Location: ../profile.php?error=incorrect_password");
-            exit();
-        }
-
         if (strlen(trim($old_pw)) == 0 || strlen(trim($new_pw)) == 0) {
             header("Location: ../profile.php?error=emptyinput");
             exit();
@@ -53,7 +36,24 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['username'])) {
         } elseif ($new_pw == $old_pw) {
             header("Location: ../profile.php?error=password_check");
             exit();
-        } 
+        } else{
+             $old_pw = md5($old_pw);
+             $new_pw = md5($new_pw);
+             $id = $_SESSION['user_id'];
+
+             $sql = "SELECT password FROM user WHERE user_id='$id' AND password='$old_pw'";
+             $result = mysqli_query($conn, $sql);
+             if (mysqli_num_rows($result) === 1) {
+
+                $new_sql = "UPDATE user SET password='$new_pw' WHERE user_id='$id'";
+                mysqli_query($conn, $new_sql);
+                header("Location: ../profile.php?success=changesuccess");
+                exit();
+             } else {
+                header("Location: ../profile.php?error=incorrect_password");
+                exit();
+             }
+        }
       
     } else {
         header("Location: ../profile.php");
