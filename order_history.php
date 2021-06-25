77881b41182cc44973aa406a0b1c20d162e02ebc
LUKAZ <lukazlim@yahoo.com>
2021-06-25 16:52:45 +0800

    Added order history page (WIP)

---
+++ order_history.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Add Stock - Admin</title>
+        
+        <?php
+        require 'modules/link.php';
+        ?>
+        
+        <link rel="stylesheet" type="text/css" href="modules/order_history.css">
+    </head>
+    
+    <?php
+    require 'modules/dbconnect.php';
+    include 'modules/header.php';
+    ?>
+    
+    <body class="bg-dark">
+        <div class="container text-white">
+            <h3 class="pt-4">Order History<span class="float-right"><a class="mb-3 btn btn-info" href="stock_levels.php"><i class="fa fa-arrow-left"></i>&nbsp; Back</a></span></h3>
+            <hr>
+            
+            
+            
+        </div>
+        
+        <script type="text/javascript">
+            
+        </script>
+    </body>
+    
+    <?php
+    include 'modules/footer.php';
+    ?>
+    
+</html>
