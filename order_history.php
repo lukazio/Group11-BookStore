c8c8b16e72e5a8db42a47c9f0e82bcc3557cf4d2
LUKAZ <lukazlim@yahoo.com>
2021-06-26 12:29:48 +0800

    Added order history search (WIP)

--- order_history.php
+++ order_history.php
@@ -19,14 +19,25 @@
     <body class="bg-dark">
         <div class="container text-white">
             <h3 class="pt-4">Order History<span class="float-right"><a class="mb-3 btn btn-info" href="stock_levels.php"><i class="fa fa-arrow-left"></i>&nbsp; Back</a></span></h3>
+            
             <hr>
             
-            
+            <div class="form-group search mb-3">
+                <span class="fa fa-search form-control-feedback"></span>
+                <input type="text" class="form-control" id="searchStock" name="searchOrder" placeholder="Search for order history">
+            </div>
             
         </div>
         
         <script type="text/javascript">
-            
+            $(document).ready(function() {
+                $('#searchOrder').on('keyup', function() {
+                    var value = $(this).val().toLowerCase();
+                    $('#ordersTable tr').filter(function() {
+                        $(this).toggle($(this).find('.title, .isbn').text().toLowerCase().indexOf(value) > -1); // Update find() after table is implemented
+                    });
+                });
+            });
         </script>
     </body>
     
