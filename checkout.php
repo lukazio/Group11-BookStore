3f164fd0ea2976a8a3ff6d70737671a6e65e6e50
vanessa <p17009280@student.newinti.edu.my>
2021-07-02 18:30:35 +0800

    include sweetalert and get grand total to check_action

--- checkout.php
+++ checkout.php
@@ -27,32 +27,56 @@
                         $sql = "SELECT username,email,address_line,city,state,zip_code,country FROM user WHERE username ='" . $_SESSION['username'] . "'";
                         $result = $conn->query($sql);
                         if ($result->num_rows > 0) {
+                            //On Success Placing Order
+                            if (isset($_GET['order']) && $_GET['order'] === "success") {
+                                echo '<script type="text/javascript">
+                                    (function() {
+                                        Swal.fire({
+                                            allowOutsideClick: false,
+                                            icon: "success",
+                                            type: "success",
+                                            timer: 2000,
+                                            title: "Order Placed!",
+                                            text: "You can now view your order details in Profile.",
+                                            showCancelButton: false,
+                                            confirmButtonText: "Back to Home",
+                                            confirmButtonColor: "#17a2b8",
+                                            reverseButtons: true
+                                        }).then((result) => {
+                                            window.location="home.php";
+                                            if(result.value){
+                                                window.location="home.php";
+                                            }
+                                        });
+                                    })();
+                                </script>';
+                            }
                             while ($row = $result->fetch_assoc()) {
                                 echo
-                                '<form class="was-validated">
+                                '<form id="checkout-form" action="action/checkout_action.php" method="post" class="was-validated">
                                 <div class = "form-row"> <div class = "form-group col-sm-6">
                                 <label for = "input-username" class = "lead">Username</label>
-                                <input type = "text" class = "form-control" id = "input-username" placeholder = "Username" value="' . $row["username"] . '"required>
+                                <input type = "text" class = "form-control" name="username" id = "input-username" placeholder = "Username" value="' . $row["username"] . '"required>
                                 </div>
                                 <div class = "form-group col-sm-6">
                                 <label for = "input-email" class = "lead">Email</label>
-                                <input type = "email" class = "form-control" id = "input-email" placeholder = "Email" value="' . $row["email"] . '" required>
+                                <input type = "email" class = "form-control" name="email" id = "input-email" placeholder = "Email" value="' . $row["email"] . '" required>
                                 </div>
                                 </div>
                                 <div class = "form-group col-mb-3">
                                 <label for = "input-address" class = "lead">Address Line</label>
-                                <textarea class = "form-control" id = "input-address" rows = "5" placeholder = "Address" required>' . $row["address_line"] . '</textarea>
+                                <textarea class = "form-control" name="address" id = "input-address" rows = "5" placeholder = "Address" required>' . $row["address_line"] . '</textarea>
                                 </div>
                                 <div class = "form-group col-mb-3">
                                 <label for = "input-city" class = "lead">City</label>
-                                <input type = "text" class = "form-control" id = "input-city" placeholder = "City"  value="' . $row["city"] . '" required>
+                                <input type = "text" class = "form-control" name="city" id = "input-city" placeholder = "City"  value="' . $row["city"] . '" required>
                                 </div>';
                                 $countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
                                 echo
                                 '<div class = "form-row">
                                 <div class = "form-group col-md-4 mb-3">
                                 <label for = "input-country" class = "lead">Country</label>
-                                <select class="form-control" id="input-country" required>';
+                                <select class="form-control" name="country" id="input-country" required>';
                                 if ($row["country"] == null) {
                                     echo'<option value = "">Select country</option>';
                                 }
@@ -70,14 +94,15 @@
                                 echo
                                 '<div class = "form-group col-md-4 mb-3">
                                 <label for = "input-state" class = "lead">State</label>
-                                <input type = "text" class = "form-control" id = "input-state" placeholder = "State"  value="' . $row["state"] . '" required>
+                                <input type = "text" class = "form-control" name="state" id = "input-state" placeholder = "State"  value="' . $row["state"] . '" required>
                                 </div>
                                 <div class = "form-group col-md-4 mb-3">
                                 <label for = "input-zip" class = "lead">Zip Code</label>
-                                <input type = "text" class = "form-control" id = "input-zip" placeholder = "Zip Code"  value="' . $row["zip_code"] . '" required>
+                                <input type = "text" class = "form-control" name="zipcode" id = "input-zip" placeholder = "Zip Code"  value="' . $row["zip_code"] . '" required>
                                 </div>
                                 </div>
-                                <button type="submit" class="btn btn-outline-success btn-lg btn-block">Confirm Order</button>
+                                <input type="hidden" id="grandTotalHidden" name="grandTotalHidden" value="" />
+                                <button id="btn-submit" type="submit" name="submit" class="btn btn-outline-success btn-lg btn-block">Confirm Order</button>
                                 </form>';
                             }
                         }
@@ -137,7 +162,7 @@
                         '<h6>Delivery Fee:</h6>' .
                         '</div>';
                         if (!isset($_SESSION['username'])) {
-                           echo '<div class="col-auto"><span class ="text-muted" id="delivery-fee">RM -</span></div>';
+                            echo '<div class="col-auto"><span class ="text-muted" id="delivery-fee">RM -</span></div>';
                         } else {
                             $sql = "SELECT country FROM user WHERE username ='" . $_SESSION['username'] . "'";
                             $result = $conn->query($sql);
@@ -229,6 +254,7 @@
             </div>
         </div>
         <!--handle changing shipping fee and grand price based on country-->
+        <!-- On Form Submit add POST of Grand Total in form action -->
         <script>
             $(document).ready(function () {
 
@@ -282,6 +308,10 @@
                     $("#grand-total").text('RM' + grand);
                 });
             });
+            $('#btn-submit').click(function () {
+                var grandtotal = $('#grand-total').text().replace("RM", "");
+                $('#grandTotalHidden').val(grandtotal);
+            });
         </script>
     </body>
 
@@ -291,4 +321,5 @@
     ?>
 
 </html>
+
 
